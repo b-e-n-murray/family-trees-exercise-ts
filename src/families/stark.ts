443ffbfdc2c544bee5bdfa527105a96d68fba3c0
Ben Murray <benmurray573@gmail.com>
2022-12-13 10:23:52 +0000

    wrote fn to log all in tree using stack method

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -2,5 +2,49 @@ import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+    const eddardsKids: Person[] = [
+        { name: "Rickon", children: [] },
+        { name: "Bran", children: [] },
+        { name: "Arya", children: [] },
+        { name: "Sansa", children: [] },
+        { name: "Robb", children: [] },
+        { name: "Jon", children: [] },
+    ]
+
+    const eddard: Person = { name: "Eddard", children: eddardsKids };
+
+    const rickardsKids: Person[] = [
+        { name: "Bengen", children: [] },
+        { name: "Lyanna", children: [] },
+        { name: "Brandon", children: [] },
+        eddard,
+    ]
+
+    const topPerson: Person = { name: "Rickard", children: rickardsKids };
+
+    return topPerson;
 }
+
+/*function logAllPeopleInTree(rootPerson)
+    workStack = empty stack of Person objects
+    push rootPerson onto workStack
+
+		while workStack is not empty: 
+        currentPerson = pop person off workStack
+        log the name property of currentPerson
+        push each element of currentPerson's children onto the workStack*/
+
+function logAllPeopleInTree(rootPerson: Person): void {
+    let workStack = [];
+    workStack.push(rootPerson)
+    while(workStack.length !== 0) {
+        let currentPerson: Person = workStack[0]
+        console.log(currentPerson.name)
+        for(let child of currentPerson.children) {
+            workStack.push(child)
+        }
+        workStack.shift()
+    }
+}
+
+logAllPeopleInTree(createStarkTree())
